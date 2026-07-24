813d98972609ea13990821e5e7f769200a52442b
hoanggyen <147621826+galina723@users.noreply.github.com>
2026-07-24 10:55:50 +0700

    feat: implement shopping cart calculation

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const calculateShoppingCart = () => {
+const calculateShoppingCart = (price, quantity, discount, tax) => {
+  
+    const subTotal = price * quantity;
+  const discountAmount = subTotal * (discount / 100);
+  const afterDiscount = subTotal - discountAmount;
+  const taxAmount = afterDiscount * (tax / 100);
+  const totalPrice = afterDiscount + taxAmount;
 
-}
-calculateShoppingCart()
+  console.log(`The money you have to pay: ${totalPrice}`);
+};
+calculateShoppingCart(100,2,10,8);
